555f4bb9755a245ee4fbe41161f76ce14897dca3
Parth <partharora99160808@gmail.com>
2020-08-14 00:25:56 +0530

    Added welcome home message on api homepage

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
-const {logErrors,genericErrorHandler} = require('./middlewares/errors');
+const { logErrors, genericErrorHandler } = require('./middlewares/errors');
 var app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
-app.use('/academia',require('./routers/academia.js'));
+app.use(express.urlencoded({ extended: true }));
+app.use('/academia', require('./routers/academia.js'));
 // app.use('/users',require('./routers/uses.js'))
+
+app.get('/', (req, res) => {
+    res.status(200).send('Okaeri');
+})
 
 app.use(logErrors);
 app.use(genericErrorHandler);
