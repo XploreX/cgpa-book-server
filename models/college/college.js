9f78c7d647ce01db4d7d2e4da257f210b3e263d9
Parth <partharora99160808@gmail.com>
2020-08-14 03:18:12 +0530

    fixed a minor thing

--- models/college/college.js
+++ models/college/college.js
@@ -1,42 +1,43 @@
 const mongoose = require('mongoose');
 const courseSchema = require('./course.js');
-const {getAbbreviation} = require('../../utility/string-helpers.js');
+const { getAbbreviation } = require('../../utility/string-helpers.js');
 const Schema = mongoose.Schema;
 
 var collegeSchema = new Schema({
-    college : {
-        type : String,
-        required : true,
-        trim : true,
-        minlength : 1,
-        unique : true ,
+    college: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        unique: true,
     },
-    abbreviation : {
-        type : String,
-        trim : true ,
-        minlength : 1
+    abbreviation: {
+        type: String,
+        trim: true,
+        minlength: 1
     },
-    courses : [courseSchema]
+    courses: [courseSchema]
 });
 
-collegeSchema.pre('save',function (next) {
-    this.abbreviation = getAbbreviation(this.college);
+collegeSchema.pre('save', function (next) {
+    if (!this.abbreviation) {
+        this.abbreviation = getAbbreviation(this.college);
+    }
     next();
 })
 
 collegeSchema.methods.courseID = function (courseName) {
-    for(let course of this.courses) {
-        if(course.course.match(courseName))
-        {
+    for (let course of this.courses) {
+        if (course.course.match(courseName)) {
             return course._id;
         }
     }
     return -1;
 }
 
-collegeSchema.methods.getCourse = function(courseName) {
-    for(let course of this.courses) {
-        if(course.course.match(courseName)) {
+collegeSchema.methods.getCourse = function (courseName) {
+    for (let course of this.courses) {
+        if (course.course.match(courseName)) {
             return course;
         }
     }
