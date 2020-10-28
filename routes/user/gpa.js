92cfe1630bad35c220e3002e2338d31accf069a4
Parth <partharora99160808@gmail.com>
2020-10-28 15:43:20 +0530

    changed to success response

--- routes/user/gpa.js
+++ routes/user/gpa.js
@@ -31,7 +31,7 @@ router.post("/gpa-data", authenticateUser, (req, res, next) => {
     return user.save();
   })
   .then((doc) => {
-    res.status(StatusCodes.OK).send(utility.response.getSucessResponse());
+    res.status(StatusCodes.OK).send(utility.response.getSuccessResponse());
   })
   .catch(next);
 });
