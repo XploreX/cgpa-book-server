9ade726af275ae2076feadb81b0fd7de20f2e043
Parth <partharora99160808@gmail.com>
2020-11-08 05:07:44 +0530

    used constant fields in get last updation of content functions

--- utility/mongoose-util/getLastListModification.js
+++ utility/mongoose-util/getLastListModification.js
@@ -1,5 +1,8 @@
+const ROOT = require(__dirname + '/../../config').ROOT;
+const academiaFields = require(ROOT + '/fields/academia');
+
 function getLastListModification() {
-    return this.lastListModification.toUTCString();
+    return this[academiaFields.TS_LAST_LIST_MODIFICATION].toUTCString();
 }
 
 module.exports = getLastListModification;
