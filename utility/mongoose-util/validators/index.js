7e7f8f54a76694fc8040ae3ea13b9b6d3770fc63
Parth <partharora99160808@gmail.com>
2021-01-16 17:19:13 +0530

    Removed redundant utility functions

--- utility/mongoose-util/validators/index.js
+++
@@ -1,5 +0,0 @@
-const ROOT = require(__dirname + '/../../../config').ROOT;
-const customRequireDirectory = require(ROOT +
-  '/utility/customRequireDirectory');
-
-module.exports = customRequireDirectory(__dirname);
