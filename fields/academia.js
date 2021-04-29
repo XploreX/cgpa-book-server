142b6bccfb1a498d496f8fa7d5b61a47f6b81163
Parth <partharora99160808@gmail.com>
2021-04-29 18:00:05 +0530

    Add relevant fields in academia field constants

--- fields/academia.js
+++ fields/academia.js
@@ -1,10 +1,29 @@
 module.exports = Object.freeze({
   TS_UPDATED_AT: 'updatedAt',
   TS_CREATED_AT: 'createdAt',
-  COLLEGE: 'college',
-  COURSE: 'course',
-  BRANCH: 'branch',
-  SEMESTER: 'semester',
-  SUBJECT: 'subject',
   TS_LAST_LIST_MODIFICATION: 'lastListModification',
+
+  COLLEGE: 'college',
+  COLLEGE_NAME_HISTORY: 'collegeNameHistory',
+  COLLEGE_ID: '_id',
+
+  COURSE: 'course',
+  COURSES: 'courses',
+  COURSE_NAME_HISTORY: 'courseNameHistory',
+  COURSE_ID: 'courseId',
+
+  BRANCH: 'branch',
+  BRANCHES: 'branches',
+  BRANCH_NAME_HISTORY: 'branchNameHistory',
+  BRANCH_ID: 'branchId',
+
+  SEMESTER: 'semester',
+  SEMESTERS: 'semesters',
+  SEMESTER_NAME_HISTORY: 'semesterNameHistory',
+  SEMESTER_ID: 'semesterId',
+
+  SUBJECT: 'subject',
+  SUBJECTS: 'subjects',
+  SUBJECT_NAME_HISTORY: 'subjectNameHistory',
+  SUBJECT_ID: 'subjectId',
 });
